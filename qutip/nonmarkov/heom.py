a276d75ec93d222f98c5dc3805627e332df07bdc
ajgpitch <alex.pitchford@gmail.com>
2016-12-23 14:34:27 +0900

    some extra stats on the L_helems added

--- qutip/nonmarkov/heom.py
+++ qutip/nonmarkov/heom.py
@@ -413,6 +413,10 @@ class HSolverDL(HEOMSolver):
         if stats: start_louvillian = timeit.default_timer()
        
         L_helems = self._add_sys_liouvillian_helems(H_sys, L_helems)
+        
+        if stats:
+            stats.add_count('L nnz', L_helems.nnz, ss_conf)
+            stats.add_count('L max matrix elem', L_helems.data.max(), ss_conf)
 
         if stats:
             stats.add_timing('Liouvillian contruct',
