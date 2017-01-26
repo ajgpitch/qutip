2badd0be6cf9292267b71255ee063d5b4cd7f00a
ajgpitch <alex.pitchford@gmail.com>
2017-01-26 15:05:26 +0000

    initial implementation of ctrlsolver

---
+++ qutip/qoc/ctrlsolver.py
@@ -0,0 +1,129 @@
+# -*- coding: utf-8 -*-
+# This file is part of QuTiP: Quantum Toolbox in Python.
+#
+#    Copyright (c) 2014 and later, Alexander J G Pitchford
+#    All rights reserved.
+#
+#    Redistribution and use in source and binary forms, with or without
+#    modification, are permitted provided that the following conditions are
+#    met:
+#
+#    1. Redistributions of source code must retain the above copyright notice,
+#       this list of conditions and the following disclaimer.
+#
+#    2. Redistributions in binary form must reproduce the above copyright
+#       notice, this list of conditions and the following disclaimer in the
+#       documentation and/or other materials provided with the distribution.
+#
+#    3. Neither the name of the QuTiP: Quantum Toolbox in Python nor the names
+#       of its contributors may be used to endorse or promote products derived
+#       from this software without specific prior written permission.
+#
+#    THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+#    "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+#    LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A
+#    PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+#    HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+#    SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+#    LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+#    DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+#    THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+#    (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+#    OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+###############################################################################
+
+# @author: Alexander Pitchford
+# @email1: agp1@aber.ac.uk
+# @email2: alex.pitchford@gmail.com
+# @organization: Aberystwyth University
+# @supervisor: Daniel Burgarth
+
+"""
+Control solver
+"""
+import os
+import numpy as np
+# QuTiP
+from qutip import Qobj
+# QuTiP logging
+import qutip.logging_utils as logging
+logger = logging.get_logger()
+
+def _is_string(var):
+    try:
+        if isinstance(var, basestring):
+            return True
+    except NameError:
+        try:
+            if isinstance(var, str):
+                return True
+        except:
+            return False
+    except:
+        return False
+
+    return False
+
+
+class ControlSolver(object):
+    """
+
+    Attributes
+    ----------
+    
+    """
+    #ToDo: Make this an abstract class
+    def __init__(self, evo_solver):
+        self.reset()
+        self.evo_solver = evo_solver
+    
+    def reset(self):
+        self.evo_solver = None
+        self.drift_dyn_gen = None
+        self.ctrl_dyn_gen = None
+        self.initial = None
+        self.target = None
+        self.clear()
+
+    def apply_params(self, params=None):
+        """
+        Set object attributes based on the dictionary (if any) passed in the
+        instantiation, or passed as a parameter
+        This is called during the instantiation automatically.
+        The key value pairs are the attribute name and value
+        Note: attributes are created if they do not exist already,
+        and are overwritten if they do.
+        """
+        if not params:
+            params = self.params
+
+        if isinstance(params, dict):
+            self.params = params
+            for key in params:
+                setattr(self, key, params[key])
+
+    @property
+    def log_level(self):
+        return logger.level
+
+    @log_level.setter
+    def log_level(self, lvl):
+        """
+        Set the log_level attribute and set the level of the logger
+        that is call logger.setLevel(lvl)
+        """
+        logger.setLevel(lvl)
+
+    def clear(self):
+        pass
+
+class ControlSolverPWC(ControlSolver):
+    
+    def __init__(self, evo_solver, tlist):
+        self.reset()
+        self.evo_solver = evo_solver
+        self.tlist = tlist
+        
+    def reset(self):
+        ControlSolver.reset(self)
+        self.tlist = None
